73ef8ac999269aa2e98f5f4658261243e89fc52a
khanh47 <phhkhanh47@gmail.com>
2023-06-08 16:06:03 +0700

    add get function to rmq2d

--- Data Structure/RMQ/RMQ2D.cpp
+++ Data Structure/RMQ/RMQ2D.cpp
@@ -4,11 +4,12 @@
 template<class X, class Y> bool maximize(X &x, Y y){ if (x < y){ x = y; return true; } return false; }
 template<class X, class Y> bool minimize(X &x, Y y){ if (x > y){ x = y; return true; } return false; }
 
-const int MAX = 1e5 + 7;
-const int LG = 20;
+const int MAX = 3e2 + 7;
+const int LG = 10;
 
 struct rmq2d{
     int table[MAX][LG][MAX][LG], row, col;
+    
     void build(){
         for (int k = 0; MASK(k) <= row; k++)
             for (int i = 1; i + MASK(k) - 1 <= row; i++)
@@ -22,5 +23,15 @@ struct rmq2d{
                         }else
                             table[i][k][j][l] = max(table[i][k - 1][j][l], table[i + MASK(k - 1)][k - 1][j][l]);
                     }
+    }    
+    
+    int get(int x, int y, int a, int b){
+        int k = __lg(a - x + 1);
+        int l = __lg(b - y + 1);
+        int mx = max({ table[x][k][y][l],
+                       table[x][k][b - MASK(l) + 1][l],
+                       table[a - MASK(k) + 1][k][y][l],
+                       table[a - MASK(k) + 1][k][b - MASK(l) + 1][l] });
+        return mx;
     }
 } rmq;
