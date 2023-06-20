7671add3e7233fa9c2c6a351a49662814700a333
khanh47 <phhkhanh47@gmail.com>
2023-06-20 10:34:26 +0700

    fix mutiple edges and change to struct

--- Graph/MaxFlow/FordFulkerson.cpp
+++ Graph/MaxFlow/FordFulkerson.cpp
@@ -1,72 +1,55 @@
-const int MAX = 1e3 + 5;
-const int INF = 1e9 + 7;
+struct MaxFlow {
+    int src, sink;
+    vector<int> edge[MAX];
+    int capacity[MAX][MAX], flow[MAX][MAX], trace[MAX];
+    bool vis[MAX];
 
-int numNode, numEdge, src, sink;
-vector<int> edge[MAX];
-int capacity[MAX][MAX], flow[MAX][MAX], trace[MAX];
-bool vis[MAX], S[MAX];
-
-bool bfs(int u){
-    memset(vis, 0, sizeof(vis));
-    queue<int> q;
-    q.push(u);
-    while (q.size()){
-        int u = q.front(); q.pop();
-        for (int v : edge[u])
-            if (!vis[v] && capacity[u][v] > flow[u][v]){
-                trace[v] = u;
-                if (v == sink)
-                    return 1;
-                vis[v] = 1;
-                q.push(v);
-            }
+    void add(int u, int v, int c){
+        if (c != capacity[u][v] && capacity[u][v])
+            return;
+        edge[u].eb(v);
+        edge[v].eb(u);
+        capacity[u][v] = c;
     }
-    return 0;
-}
 
-void increase(){
-    int mn = INF;
-    int u = sink;
-    while (u != src){
-        int pre = trace[u];
-        minimize(mn, capacity[pre][u] - flow[pre][u]);
-        u = pre;
+    bool bfs(){
+        memset(vis, 0, sizeof(vis));
+        queue<int> q;
+        q.push(src);
+        while (q.size()){
+            int u = q.front(); q.pop();
+            for (int v : edge[u])
+                if (!vis[v] && capacity[u][v] > flow[u][v]){
+                    vis[v] = 1;
+                    trace[v] = u;
+                    if (v == sink)
+                        return 1;
+                    q.push(v);
+                }
+        }
+        return 0;
     }
-    u = sink;
-    while (u != src){
-        int pre = trace[u];
-        flow[pre][u] += mn;
-        flow[u][pre] -= mn;
-        u = pre;
-    }
-}
 
-int maxFlow(){
-    while (bfs(src))
-        increase();
-    int res = 0;
-    for (int v : edge[src])
-        res += flow[src][v];
-    return res;
-}
-
-vector<int> minCut(){
-    vector<int> V;
-    vector<pair<int, int>> res;
-    S[src] = 1;
-    queue<int> q;
-    q.push(src);
-    while (q.size()){
-        int u = q.front(); q.pop();
-        for (int v : edge[u])
-            if (!S[v] && capacity[u][v] > flow[u][v])
-                S[v] = 1,
-                V.eb(v),
-                q.push(v);
+    void increase(){
+        int mn = INF, pre;
+        for (int u = sink; u != src; u = pre){
+            pre = trace[u];
+            minimize(mn, capacity[pre][u] - flow[pre][u]);
+        }
+        for (int u = sink; u != src; u = pre){
+            pre = trace[u];
+            flow[pre][u] += mn;
+            flow[u][pre] -= mn;
+        }
     }
-    for (int u : V)
-        for (int v : edge[u])
-            if (!S[v])
-                res.pb({u, v});
-    return res;
-}
+
+    ll maxFlow(int _src, int _sink){
+        src = _src; sink = _sink;
+        while (bfs())
+            increase();
+        ll res = 0;
+        for (int v : edge[src])
+            res += flow[src][v];
+        return res;
+    }
+} flow;
