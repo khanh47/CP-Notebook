c73d175c4c242e7b6631b16b5f7c30250d39ba87
khanh47 <phhkhanh47@gmail.com>
2023-12-15 08:05:12 +0700

    fix kmp

--- String Processing/KMP.cpp
+++ String Processing/KMP.cpp
@@ -1,5 +1,11 @@
 
+string s, t;
+int nxt[MAX][26];
+
+void solve(){
     memset(nxt, 0, sizeof(nxt));
+    cin >> s >> t;
+    int n = s.size(), m = t.size();
     FOR (i, 0, m - 2){
         int pre = nxt[i][t[i] - 'a'];
         nxt[i][t[i] - 'a'] = i + 1;
@@ -7,3 +13,4 @@
             nxt[i + 1][j] = nxt[pre][j];
     }
     nxt[m - 1][t[m - 1] - 'a'] = m;
+}
