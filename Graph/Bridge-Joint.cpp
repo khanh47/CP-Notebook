6f5895b4bf071d560f7cf8fe7f8906d676e36845
khanh47 <phhkhanh47@gmail.com>
2024-01-04 18:18:30 +0700

    update graph

--- Graph/Bridge-Joint.cpp
+++ Graph/Bridge-Joint.cpp
@@ -1,26 +1,36 @@
-
-int num[10004], low[10004], joint[10004];
-vector<int> g[10004];
-int timeDfs, c;
+int numNode, numEdge, cnt, bridge;
+vector<int> edge[MAX];
+bool art[MAX];
+int num[MAX], low[MAX];
 
 void dfs(int u, int pre){
-    num[u] = low[u] = ++timeDfs;
+    num[u] = low[u] = ++cnt;
     int child = 0;
-    for(int v : g[u]){
+    for (int v : edge[u]){
         if (v == pre)
             continue;
-        if(!num[v]){
-            dfs(v, u);
-            low[u] = min(low[u], low[v]);
-            child++;
-            if (low[v] == num[v])
-                c++;
-            if (u == pre){
-                if (child > 1)
-                    joint[u] = 1;
-            }else if (low[v] >= num[u])
-                joint[u] = 1;
-        }else
-            low[u] = min(low[u], num[v]);
+        if (num[v]){
+            minimize(low[u], num[v]);
+            continue;
+        }
+        dfs(v, u);
+        child++;
+        minimize(low[u], low[v]);
+        if (pre < 0){
+            if (child > 1)
+                art[u] = 1;
+        }else if (low[v] >= num[u])
+            art[u] = 1;
+        bridge += num[v] == low[v];
     }
+}
+
+void solve(){
+    int joint = 0;
+    FOR (i, 1, numNode){
+        if (!num[i])
+            dfs(i, -1);
+        joint += art[i];
+    }
+    cout << joint << ' ' << bridge;
 }
